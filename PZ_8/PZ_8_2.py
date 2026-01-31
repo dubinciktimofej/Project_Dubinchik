c20dc66625c865f9ff3cac83dc290393086d2cf9
Тимофей <dubinciktimofej18@gmail.com>
2026-01-31 12:44:51 +0300

    PZ_8_2 исправленная

--- PZ_8/PZ_8_2.py
+++ PZ_8/PZ_8_2.py
@@ -12,16 +12,5 @@ def sum_halves(d):
     print("Сумма первой половины:", sum1)
     print("Сумма второй половины:", sum2)
 
-
-<<<<<<< HEAD
 slovarik = {"a": 52, "chetverg": 100, "python": 13, "end": 27}
-=======
-slovarik = {
-    "a": 52,
-    "chetverg": 100,
-    "python": 13,
-    "end": 27
-}
->>>>>>> 8fb0a923fab7c0a1561bae3d061e4cf9f635b7bd
-
 sum_halves(slovarik)
