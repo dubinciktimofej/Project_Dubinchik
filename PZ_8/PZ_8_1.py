e375affceca7bea9ece145d012230a9860e4c663
Тимофей <dubinciktimofej18@gmail.com>
2026-03-06 22:10:27 +0300

    PZ_5 recommit

--- PZ_8/PZ_8_1.py
+++ PZ_8/PZ_8_1.py
@@ -1,8 +1,7 @@
 #Вариант 4
 #Найдите ключ с минимальным значением в sample_dict = {'Physics': 82, 'Math': 65,
 #'history': 75}
-sample_dict = {'Physics': 82, 'Math': 65,
-'history': 75}
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
 print(sample_dict)
 min_value = min(sample_dict.items())
-print("Ключ с минимальным значением в данном словаре:", min_value)
+print("Ключ с минимальным значением в данном словаре: ", min_value)
