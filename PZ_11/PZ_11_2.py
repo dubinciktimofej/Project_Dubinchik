dd579e4ce76d51e317ad645069669627716d5b3c
Тимофей <dubinciktimofej18@gmail.com>
2026-06-24 01:42:36 +0300

    PZ_11 recommit

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -1,9 +1,8 @@
-text:
+#Вариант 4. Составить генератор(yield), который выводит из строки только буквы.
+def letters_generator(text):
+    for char in text:
         if char.isalpha():
             yield char
-#Составить генератор(yield), который выводит из строки только буквы.
-def letters_generator(text):
-    for char in 
 text = "По четвергам рано вставать не надо 184396"
 for letter in letters_generator(text):
     print(letter, end="")
